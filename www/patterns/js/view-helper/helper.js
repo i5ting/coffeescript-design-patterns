f51ad163d4c72f4853a31023cac8fb1736ef8acf
Dustin Boston <superborkado@gmail.com>
2012-07-19 23:18:31 -0400

    reorg

--- www/patterns/js/view-helper/helper.js
+++
@@ -1,8 +0,0 @@
-/*global define*/
-define(["./registry"], function (registry) {
-    return {
-        getRequest: function () {
-            return registry.getRequest();
-        }
-    };
-});
--- www/patterns/js/view-helper/registry.js
+++
@@ -1,9 +0,0 @@
-/*global define*/
-define({
-    getRequest: function () {
-        return {
-            title: "View Helper",
-            link: "http://java.sun.com/blueprints/corej2eepatterns/Patterns/ViewHelper.html"
-        };
-    }
-});
